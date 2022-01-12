21835de9ebdd3cd697559a36f79f9d07ba4ac167
AnadelaJara <anadelajara@gmail.com>
2022-01-12 11:14:31 +0100

    add toy ex

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,14 @@
 import '../styles/App.scss';
+import {useState} from 'react';
 
 function App() {
+
+  const [numberOfErrors, setnumberOfErrors] = useState (0);
+
+  const handleClickBtn = () => {
+    setnumberOfErrors (numberOfErrors+1);
+    console.log (numberOfErrors);
+  }
  
   return (
     <div className="page">
@@ -46,7 +54,7 @@ function App() {
             />
           </form>
         </section>
-        <section className="dummy error-5">
+        <section className={"dummy error-" + numberOfErrors}>
           <span className="error-13 eye"></span>
           <span className="error-12 eye"></span>
           <span className="error-11 line"></span>
@@ -59,8 +67,11 @@ function App() {
           <span className="error-4 line"></span>
           <span className="error-3 line"></span>
           <span className="error-2 line"></span>
+          <span className="error-2 line"></span>
           <span className="error-1 line"></span>
         </section>
+        <button onClick={handleClickBtn}>Incrementar</button>
+
       </main>
     </div>
   );
