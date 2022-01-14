eaea07ee1d4752505a75eb8eb50e849e2bedac1f
Mara Rocha Fernández <maranhadisenha@gmail.com>
2022-01-14 14:30:33 +0100

    add user letters and solution underscores

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ Módulo 3: React
 
 # Ejercicio de pair programming
 
+**URL del ejercicio: [Hangman Game](http://beta.adalab.es/promo-o-module-3-pair-4-sprint-1-hangman-game/)**
+
 ## Pareja 4 / Sprint 1
 
 - [Ana de la Jara](https://github.com/AnadelaJara)
@@ -15,3 +17,9 @@ Módulo 3: React
 - [x] Ejercicio: el muñeco
 - [x] Ejercicio: controlar el input
 - [x] Ejercicio: letras permitidas
+- [x] Ejercicio: pintar los guiones de la solución (fase 1)
+- [x] Ejercicio: modificar un array del estado
+- [ ] Ejercicio: pintar los guiones de la solución (fase 2)
+- [ ] Ejercicio: pintar las letras falladas
+- [ ] Ejercicio: pintar el muñeco
+- [ ] Ejercicio: obtener una palabra aleatoria de un API
--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,58 @@
 import '../styles/App.scss';
 import { useState } from 'react';
 
+// Ejercicio 3: pintar los guiones de la solución (fase 2)
+// - en renderSolutionLetters:
+// - si la letra de word no está en userLetters: <li className="letter"></li>
+// - si la letra de word sí está en userLetters: <li className="letter">{letra}</li>
+// - pista: .includes() ???
+
+// Ejercicio 4: pintar las letras falladas
+// - crear función renderErrorLetters
+// - ejecutarla en retorno del componente App debajo del título "Letras falladas"
+// - en renderErrorLetters:
+// - filtrar las letras de word que no existen en userLetters
+// - recorrer letras filtradas con map para pintar <li className="letter">{letra}</li>
+
+// Ejercicio 5: pintar el muñeco
+// calcular nº de errores con filter
+// actualizar css del muñeco
+
 function App() {
   const [numberOfErrors, setNumberOfErrors] = useState(0);
   const [lastLetter, setLastLetter] = useState('');
+  const [word, setWord] = useState('katakroker');
+  const [userLetters, setUserLetters] = useState([]);
 
   const handleClickBtn = () => {
     setNumberOfErrors(numberOfErrors + 1);
   };
 
   const handleInput = (event) => {
-    const inputValue = event.target.value;
-    setLastLetter(inputValue.match(/[a-z]/gi));
+    let inputValue = event.target.value;
+    setLastLetter(inputValue);
+
+    if (inputValue) {
+      inputValue = event.target.value.toLowerCase().match('[A-zÁ-úÄ-üñÑ]');
+
+      if (inputValue) {
+        const foundLetter = userLetters.find(
+          (letter) => letter === inputValue[0]
+        );
+
+        if (!foundLetter) {
+          userLetters.push(inputValue[0]);
+          setUserLetters([...userLetters]);
+        }
+      }
+    }
+  };
+
+  const renderSolutionLetters = () => {
+    const wordLetters = word.split('');
+    return wordLetters.map((letter, index) => {
+      return <li key={index} className="letter"></li>;
+    });
   };
 
   return (
@@ -23,18 +64,7 @@ function App() {
         <section>
           <div className="solution">
             <h2 className="title">Solución:</h2>
-            <ul className="letters">
-              <li className="letter">k</li>
-              <li className="letter">a</li>
-              <li className="letter"></li>
-              <li className="letter">a</li>
-              <li className="letter">k</li>
-              <li className="letter">r</li>
-              <li className="letter"></li>
-              <li className="letter">k</li>
-              <li className="letter">e</li>
-              <li className="letter">r</li>
-            </ul>
+            <ul className="letters">{renderSolutionLetters()}</ul>
           </div>
           <div className="error">
             <h2 className="title">Letras falladas:</h2>
